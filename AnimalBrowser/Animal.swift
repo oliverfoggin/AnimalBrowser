e87df75b8c709b0000daec99c01c3ff9aeed1163
Oliver Foggin <oliverfoggin@me.com>
2015-06-17 23:50:07 +0200

    Updated to use map instead of for loop.

--- AnimalBrowser/Animal.swift
+++ AnimalBrowser/Animal.swift
@@ -11,29 +11,18 @@ import Foundation
 final class AnimalDataSource {
     var animals: [Animal] = {
         guard let path = NSBundle.mainBundle().pathForResource("Animals", ofType: "plist"),
-            animalArray = NSArray(contentsOfFile: path) else {return []}
-        
-        var array = [Animal]()
-        
-        for a in animalArray {
-            guard let name = a["name"] as? String,
-                latinName = a["species"] as? String,
-                text = a["text"] as? String,
-                authorName = a["authorName"] as? String,
-                authorPhotoName = a["authorPhoto"] as? String else {continue}
-            
-            let imageName = a["photo"] as? String
-            
-            array.append(Animal(name: name,
-                latinName: latinName,
-                text: text,
-                imageName: imageName,
-                authorName: authorName,
-                authorPhotoName: authorPhotoName))
-        }
-        
-        return array.sort{$0 < $1}
-        }()
+            animalArray = NSArray(contentsOfFile: path) as? [[String: String]] else {return []}
+
+        return animalArray.map {
+            a in
+            return Animal(name: a["name"]!,
+                latinName: a["species"]!,
+                text: a["text"]!,
+                imageName: a["photo"],
+                authorName: a["authorName"]!,
+                authorPhotoName: a["authorPhoto"]!)
+        }.sort{$0 < $1}
+    }()
 }
 
 struct Animal {
